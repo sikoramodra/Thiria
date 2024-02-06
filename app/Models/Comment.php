9f3c983bcfbdd31a0b8c876691480611957656ad
sikoramodra <wojciechmodro@gmail.com>
2024-02-06 20:22:36 +0100

    fix: recursive comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -32,7 +33,7 @@ class Comment extends Model {
         return $this->belongsTo(User::class, 'author_id');
     }
 
-    public function reply(): BelongsTo {
-        return $this->belongsTo(Comment::class, 'parent_id');
+    public function replies(): HasMany {
+        return $this->hasMany(Comment::class, 'parent_id');
     }
 }
--- resources/views/publication/show.blade.php
+++ resources/views/publication/show.blade.php
@@ -70,54 +70,8 @@
 
         <div>
             @foreach ($comments as $comment)
-                @if(!isset($comment->deleted_at) && !isset($comment->parent_id))
-                    <div class="p-4 my-4 bg-white border border-dark-500 rounded-lg shadow dark:bg-dark-100 dark:border-dark-200 ">
-                        <div class="flex mb-4">
-                            <i data-feather="user" class="h-10 w-auto border-2 rounded-lg mr-2"></i>
-                            <div class="flex flex-col">
-                                <p class="text-sm font-bold">{{ $comment->author->name }}</p>
-                                <p class="text-sm dark:text-dark-500">{{ $comment->updated_at }}</p>
-                            </div>
-                            <i data-feather="more-horizontal" class="cursor-pointer ml-auto"></i>
-                        </div>
-
-                        <p class="text-md dark:text-dark-500">
-                            {{ $comment->content }}
-                        </p>
-
-                        <button class="w-20 text-white bg-green-500 hover:bg-green-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-500 dark:hover:bg-green-400" onclick="setRespond({{$comment->id}},'{{ $comment->author->name }}')">Respond</button>
-
-                        <form action="{{ route('comment.delete', ['comment' => $comment]) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button class="w-20 text-white bg-red-500 hover:bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-400">Delete</button>
-                        </form>
-                        @foreach ($comments as $respond)
-                            @if($respond->parent_id==$comment->id)
-                                    <div class="my-4 p-4 rounded-lg ml-4 bg-white border border-dark-500 dark:bg-dark-100 dark:border-dark-200">
-                                        <div class="flex mb-4">
-                                            <i data-feather="user" class="h-10 w-auto border-2 rounded-lg mr-2"></i>
-                                            <div class="flex flex-col">
-                                                <p class="text-sm font-bold">{{ $comment->author->name }}</p>
-                                                <p class="text-sm dark:text-dark-500">{{ $comment->updated_at }}</p>
-                                            </div>
-                                            <i data-feather="more-horizontal" class="cursor-pointer ml-auto"></i>
-                                        </div>
-
-                                        <p class="text-md dark:text-dark-500">
-                                            {{ $respond->content }}
-                                        </p>
-                                    </div>
-                            @endif
-                        @endforeach
-
-                    </div>
-                @elseif (!isset($comment->parent_id))
-                    <div class="p-4 my-4 bg-white border border-dark-500 rounded-lg shadow dark:bg-dark-100 dark:border-dark-200 ">
-                        <p class="text-md dark:text-dark-500">
-                            [deleted]
-                        </p>
-                    </div>
+                @if(!isset($comment->parent_id))
+                    <x-comment :comment="$comment" :comments="$comments"></x-comment>
                 @endif
             @endforeach
         </div>
