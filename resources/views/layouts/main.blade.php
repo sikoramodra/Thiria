70ee513e11a04f9b93df2c7685e1e2b6bcd4d609
sikoramodra <wojciechmodro@gmail.com>
2024-03-28 09:37:59 +0100

    chore: reformat php files

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="pl" class="dark">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>@yield('title')</title>
-        @vite('resources/css/app.css')
-    </head>
-    <body>
-        @yield('content')
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+@yield('content')
+</body>
 </html>
